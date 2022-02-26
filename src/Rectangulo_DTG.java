af3981ea317958a921112923cfe863c47c82c2fe
DanielTrGa <danieltriguerogalvez@hotmail.com>
2022-02-26 17:43:07 +0100

    versión comentada

--- src/Rectangulo_DTG.java
+++ src/Rectangulo_DTG.java
@@ -1,4 +1,7 @@
-
+/**
+ * @author Daniel TG
+ * @version 1.2
+ */
 public class Rectangulo_DTG extends FiguraGeometrica_DTG {
 	private double l1;
 	private double l2;
@@ -9,11 +12,21 @@ public class Rectangulo_DTG extends FiguraGeometrica_DTG {
 		l2 = lP;
 	}
 
+	
+	/** 
+	 * multiplica lado por lado
+	 * @return double (lado por lado)
+	 */
 	@Override
 	public double area() {
 		return l1 * l2;
 	}
 	
+	
+	/** 
+	 * saca el perimetro del rectangulo
+	 * @return double ((lado*2)+(lado*2))
+	 */
 	@Override
 	public double perimetro() {
 		return (2 * l1) + (2 * l2);
